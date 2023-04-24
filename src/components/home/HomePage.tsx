a7ef594b3e09597325039bd35a09f07c8c70940e
phamhuutriet <triet.ph1999@gmail.com>
2023-04-23 21:30:06 -0600

    feat: add conditional render for home page

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+enum HomeRenderType {
+  TEXT_INPUT,
+  GENERATED_CARDS,
+}
 
 export const HomePage = () => {
-  return (
-    <>
-      <h1>This is the home page</h1>
-    </>
-  );
+  const [currentRenderType, setCurrentRenderType] = useState<HomeRenderType>(HomeRenderType.TEXT_INPUT);
+
+  switch (currentRenderType) {
+    case HomeRenderType.TEXT_INPUT:
+      return <TextInput />;
+    case HomeRenderType.GENERATED_CARDS:
+      return <GeneratedCards />;
+    default:
+      return <TextInput />;
+  }
+};
+
+const TextInput = () => {
+  return <h1>Text input</h1>;
+};
+
+const GeneratedCards = () => {
+  return <h1>Generated Cards</h1>;
 };
