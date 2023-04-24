0af68e6135c966cd0c7b02eb6bd58db02ab3b005
phamhuutriet <triet.ph1999@gmail.com>
2023-04-23 19:46:45 -0600

    feat: set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./components/home/HomePage";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/test" exact component={TestPage} /> */}
+      </Routes>
+    </Router>
   );
 }
+
+const TestPage = () => {
+  return (
+    <>
+      <h1>This is the test page</h1>
+    </>
+  );
+};
 
 export default App;
