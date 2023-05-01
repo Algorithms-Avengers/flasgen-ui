ab2618c4b1d3ee19c6dc7ae1049616679ea36146
phamhuutriet <triet.ph1999@gmail.com>
2023-05-01 09:51:25 -0600

    feat: add generated card component

--- src/components/home/GeneratedCards.tsx
+++ src/components/home/GeneratedCards.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlashCardData } from "../data/FlashCardData";
+import { Button } from "@mui/material";
 
 type GeneratedCardsProps = {
   onChangePage: Function;
@@ -10,9 +11,68 @@ export const GeneratedCards = ({ onChangePage, flashcards }: GeneratedCardsProps
   console.log("Flash cards: ", flashcards);
 
   return (
-    <>
-      <h1>Generated Card</h1>
-      <button onClick={() => onChangePage()}>Change Route</button>
-    </>
+    <div style={{ textAlign: "center" }}>
+      <h1>Generated Cards</h1>
+      <ScrollableBox maxHeight="400px">
+        <ol>
+          {flashcards.map((flashcard: FlashCardData, index) => (
+            <li>
+              <RowFlashCard flashcard={flashcard} key={index} />
+            </li>
+          ))}
+        </ol>
+      </ScrollableBox>
+      <br />
+      <Button variant="contained" color="success" onClick={() => onChangePage()}>
+        SAVE CARDS
+      </Button>
+      <br />
+      <br />
+      <Button variant="contained" color="secondary" onClick={() => onChangePage()}>
+        REGENERATE
+      </Button>
+    </div>
+  );
+};
+
+interface ScrollableBoxProps {
+  children: React.ReactNode;
+  maxHeight: string;
+}
+
+const ScrollableBox = ({ children, maxHeight }: ScrollableBoxProps) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ maxHeight: maxHeight, overflowY: "scroll", border: "1px solid black", padding: "10px", width: "80%", maxWidth: "800px", height: "600px" }}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollableBox;
+
+interface RowFlashCardProps {
+  flashcard: FlashCardData;
+}
+
+const RowFlashCard = ({ flashcard }: RowFlashCardProps) => {
+  const MAX_LENGTH = 50;
+  const optionalDots = flashcard.question.length >= MAX_LENGTH ? "...?" : "";
+
+  return (
+    <div style={{ display: "flex", marginLeft: "20px" }}>
+      <div style={{ display: "flex", flex: 1, fontSize: "1.2em" }}>
+        <p>
+          {flashcard.question.slice(0, MAX_LENGTH)}
+          {optionalDots}
+        </p>
+      </div>
+      <div style={{ marginLeft: "10px", marginRight: "20px" }}>
+        <Button variant="contained" color="primary">
+          SHOW
+        </Button>
+      </div>
+    </div>
   );
 };
