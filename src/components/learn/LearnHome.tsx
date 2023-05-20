1a5323e2b3f90592ef9e0866e030876f2aca6407
phamhuutriet <triet.ph1999@gmail.com>
2023-05-20 12:58:10 -0600

    add statistic feature and learn all feature

--- src/components/learn/LearnHome.tsx
+++ src/components/learn/LearnHome.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import { Button } from "@mui/material";
+import { LearnContext, LearnContextType } from "components/data/DataContext";
+import { fetchTodayLearnFlashcards, FlashCardData } from "components/data/FlashCardData";
+import { RouteEnum } from "components/navigation/NavigationBar";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import "./CardStatistic.css";
 
 export const LearnHome = () => {
+  const [todayLearnCards, setTodayLearnCards] = useState<FlashCardData[]>([]);
+  const { setLearnDeck } = useContext(LearnContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedCards = await fetchTodayLearnFlashcards();
+      setTodayLearnCards(fetchedCards);
+    };
+
+    fetchData().catch();
+  }, []);
+
+  const onClickLearnNow = () => {
+    setLearnDeck((prevState: LearnContextType) => ({ ...prevState, flashCards: todayLearnCards }));
+    navigate(RouteEnum.LEARN_DECK);
+  };
+
   return (
-    <>
-      <h1>This is the learn home</h1>
-    </>
+    <div className="card-statistics-container">
+      <h1 className="card-statistics-title">LEARN CARDS</h1>
+      <div className="card-statistics-box">
+        <p>Total number of cards: {todayLearnCards.length}</p>
+        <p>Cards remain: {todayLearnCards.length}</p>
+        <p>Cards learned: 0</p>
+      </div>
+      <div className="button-container">
+        <Button variant="outlined" onClick={() => onClickLearnNow()}>
+          LEARN NOW
+        </Button>
+      </div>
+    </div>
   );
 };
